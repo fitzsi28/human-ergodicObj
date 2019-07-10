1d740c7a325e378e88a6a6be214f8728796a6f97
kt-fitz <fitzsi28@gmail.com>
2019-07-10 15:48:35 -0500

    replaced for with while loop and avoided checking edge points

--- edgedetect.cpp
+++ edgedetect.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/opencv.hpp>//namespace cv
 using namespace std;
 const int SEARCHRAD = 50;
+const int THRESHOLD = 130;
 
 double euclidist (int x, int y, int xt, int yt){
   double d= sqrt(pow(x-xt,2.)+pow(y-yt,2.));
@@ -20,37 +21,32 @@ int main()
   cv::Mat imagetemp = cv::imread(imageName.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
   image = (cv::Scalar::all(255)-imagetemp);//cout<<cv::mean(image)[0]<<"\n";
   //cv::flip(image,image,-1);
- /*for(int j=1;j<4;j++){
-      for(int m=-j;m<=j;m++){
-      cout<<"("<<m<<","<<-j<<")";
-      cout<<"("<<m<<","<<j<<")";
-      };
-     for(int n=-j+1;n<j;n++){
-      cout<<"("<<-j<<","<<n<<")";
-      cout<<"("<<j<<","<<n<<")";
-      };
-    cout<<"\n";
-    };*/
   random_device rd; mt19937 eng(rd());
-  uniform_int_distribution<> distr(SEARCHRAD,2200-1);
+  uniform_int_distribution<> distr(0,2200);//-SEARCHRAD);
+  //cout<<"\n"<<(double)imagetemp.at<uchar>(2200,distr(eng))<<"\n";
+  //cout<<(double)imagetemp.at<uchar>(0,10)<<"\n";
   for(int i=0;i<=100000;i++){
     int x = distr(eng);
     int y = distr(eng);
-    //int j = 1;
+    int j = 1;
     bool found = false;
-    for(int j=1;j<=SEARCHRAD;j++){//while(found ==false or j>10)
+    while(found==false and j<=SEARCHRAD){//for(int j=1;j<=SEARCHRAD;j++){
       for(int m=-j;m<=j;m++){
-          //cout<<"("<<y+m<<", "<<x-j<<"): ";
-          //cout<<(double)image.at<uchar>(y+m,x-j);
-      if(image.at<uchar>(y+m,x-j)>200){imagetemp.at<uchar>(y,x)=0;found=true;};
-      if(image.at<uchar>(y+m,x+j)>200){imagetemp.at<uchar>(y,x)=0;found = true;};
+        if(y+m>=image.rows or y+m<=0 or x-j>=image.cols or x-j<=0 or x+j>=image.cols or x+j<=0){//do nothing
+        }else {
+          if(image.at<uchar>(y+m,x-j)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found=true;};
+          if(image.at<uchar>(y+m,x+j)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found = true;};
+        };
       };
       for(int n=-j+1;n<j;n++){
-        if(image.at<uchar>(y-j,x+n)>200){imagetemp.at<uchar>(y,x)=0;found=true;};
-        if(image.at<uchar>(y+j,x+n)>200){imagetemp.at<uchar>(y,x)=0;found = true;};
+        if(y-j>=image.rows or y-j<=0 or y+j>=image.rows or y+j<=0 or x+n>=image.cols or x+n<=0){//do nothing
+        }else {
+          if(image.at<uchar>(y-j,x+n)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found=true;};
+          if(image.at<uchar>(y+j,x+n)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found = true;};
+        };
       };
       
-      //j++;    
+      j++;    
     /*if(image.at<uchar>(y,x)<200){
       imagetemp.at<uchar>(y,x)=0;
     };*/
