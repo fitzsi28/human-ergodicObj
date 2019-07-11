1fa149718a6c5971d3d2c0e6d31072e458e6a79e
kt-fitz <fitzsi28@gmail.com>
2019-07-11 17:53:44 -0500

    tracking distance and nearest black pixel

--- edgedetect.cpp
+++ edgedetect.cpp
@@ -2,10 +2,10 @@
 #include<string>
 #include <fstream>
 #include<math.h>
-//#include<armadillo>
+#include<armadillo>
 #include <opencv2/opencv.hpp>//namespace cv
 using namespace std;
-const int SEARCHRAD = 50;
+const int SEARCHRAD = 100;
 const int THRESHOLD = 130;
 
 double euclidist (int x, int y, int xt, int yt){
@@ -19,38 +19,51 @@ int main()
   cv::Mat image;
   string imageName("apple.png");
   cv::Mat imagetemp = cv::imread(imageName.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
-  image = (cv::Scalar::all(255)-imagetemp);//cout<<cv::mean(image)[0]<<"\n";
+  image =(cv::Scalar::all(255)-imagetemp);
   //cv::flip(image,image,-1);
   random_device rd; mt19937 eng(rd());
-  uniform_int_distribution<> distr(0,2200);//-SEARCHRAD);
-  //cout<<"\n"<<(double)imagetemp.at<uchar>(2200,distr(eng))<<"\n";
-  //cout<<(double)imagetemp.at<uchar>(0,10)<<"\n";
+  uniform_int_distribution<> distr(0,2200-1);
   for(int i=0;i<=100000;i++){
     int x = distr(eng);
     int y = distr(eng);
-    int j = 1;
-    bool found = false;
-    while(found==false and j<=SEARCHRAD){//for(int j=1;j<=SEARCHRAD;j++){
+    int xnear =0,ynear=0;
+    double distnear;
+    int j = 1; bool found = false;
+    while(found==false and j<=SEARCHRAD){
+      arma::vec dist = 2200*arma::ones<arma::vec>(8*j);
+      arma::mat pixels = arma::zeros<arma::mat>(8*j,2);
       for(int m=-j;m<=j;m++){
         if(y+m>=image.rows or y+m<=0 or x-j>=image.cols or x-j<=0 or x+j>=image.cols or x+j<=0){//do nothing
         }else {
-          if(image.at<uchar>(y+m,x-j)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found=true;};
-          if(image.at<uchar>(y+m,x+j)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found = true;};
+          if(image.at<uchar>(y+m,x-j)>THRESHOLD){found=true;
+            dist = arma::shift(dist,1);dist(0) = euclidist(x,y,x-j,y+m);
+            pixels = arma::shift(pixels,1,0); pixels(0,0)=x-j; pixels(0,1)=y+m;};
+          if(image.at<uchar>(y+m,x+j)>THRESHOLD){found = true;
+            dist = arma::shift(dist,1);dist(0) = euclidist(x,y,x+j,y+m);
+            pixels = arma::shift(pixels,1,0); pixels(0,0)=x+j; pixels(0,1)=y+m;};
         };
       };
       for(int n=-j+1;n<j;n++){
         if(y-j>=image.rows or y-j<=0 or y+j>=image.rows or y+j<=0 or x+n>=image.cols or x+n<=0){//do nothing
         }else {
-          if(image.at<uchar>(y-j,x+n)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found=true;};
-          if(image.at<uchar>(y+j,x+n)>THRESHOLD){imagetemp.at<uchar>(y,x)=0;found = true;};
+          if(image.at<uchar>(y-j,x+n)>THRESHOLD){found=true;
+            dist = arma::shift(dist,1);dist(0) = euclidist(x,y,x+n,y-j);
+            pixels = arma::shift(pixels,1,0); pixels(0,0)=x+n; pixels(0,1)=y-j;};
+          if(image.at<uchar>(y+j,x+n)>THRESHOLD){found = true;
+            dist = arma::shift(dist,1);dist(0) = euclidist(x,y,x+n,y+j);
+            pixels = arma::shift(pixels,1,0); pixels(0,0)=x+n; pixels(0,1)=y+j;};
         };
       };
-      
+      xnear = pixels(dist.index_min(),0);
+      ynear = pixels(dist.index_min(),1);
+      distnear = euclidist(x,y,xnear,ynear);
       j++;    
-    /*if(image.at<uchar>(y,x)<200){
-      imagetemp.at<uchar>(y,x)=0;
-    };*/
-    };  
+    };
+    if(found==false){}//imagetemp.at<uchar>(y,x)=0;}
+    else{
+      if(distnear>50.){imagetemp.at<uchar>(y,x)=0;}
+      //cout<<" ("<<x<<","<<y<<") "<<" ("<<xnear<<","<<ynear<<")\n";
+      };
   };
  //cv::namedWindow(window_name1,cv::WINDOW_NORMAL);
  cv::namedWindow(window_name2,cv::WINDOW_NORMAL);
