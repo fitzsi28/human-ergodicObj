927870951b092ce79365bc86cd3ded3a795ef1bc
kt-fitz <fitzsi28@gmail.com>
2019-08-15 17:35:08 -0500

    q(s) function setup

--- SAC_MDA/dkl_cost.hpp
+++ SAC_MDA/dkl_cost.hpp
@@ -10,21 +10,25 @@ class dklcost {
   double L1,L2,T;
   int X1,X2;//index of relavant dimensions in xvector
   int K;//number of samples in the search domain
-  inline double trapint(std::function<double(double,double)> f){
+  inline double trapint(std::function<double(const arma::vec&)> f){
     double total = 0.,x0,y0,xf,yf;
     //x0=-L1,y0=-L2,xf=L1,yf=L2;
     x0=0.,y0=0.,xf=2*L1,yf=2*L2;
     int N1=50,N2=50; double d1=(2.*L1/N1);double d2=(2.*L2/N2);
-    total = (d1*d2/4)*(f(x0,y0)+f(x0,yf)+f(xf,y0)+f(xf,yf));
+    arma::vec s1={x0,y0}, s2={x0,yf}, s3={xf,y0}, s4={xf,yf};
+    total = (d1*d2/4)*(f(s1)+f(s2)+f(s3)+f(s4));
     for(int i = 1; i<N1;i++){
-      total+=(d1*d2/2)*(f(x0+i*d1,y0)+f(x0+i*d1,yf));
+      s1(0)=x0+i*d1; s1(1)=y0; s2(0)=x0+i*d1; s2(1)=yf;
+      total+=(d1*d2/2)*(f(s1)+f(s2));
     }
     for(int j = 1; j<N2;j++){
-      total+=(d1*d2/2)*(f(x0,y0+j*d2)+f(xf,y0+j*d2));
+      s1(0)=x0; s1(1)=y0+j*d2; s2(0)=xf; s2(1)=y0+j*d2;
+      total+=(d1*d2/2)*(f(s1)+f(s2));
     }
     for(int k=1; k<N2;k++){
       for(int j=1;j<N1;j++){ 
-        total+=d1*d2*f(x0+k*d1,y0+j*d2);
+        s1(0)=x0+k*d1; s1(1)=y0+j*d2;
+        total+=d1*d2*f(s1);
       }
     }
   return total;};
@@ -39,17 +43,23 @@ class dklcost {
     return total;};*/
     
   public:
-    arma::mat xpast;
+    arma::mat xpast,xfuture;
     arma::mat domainsamps;
+    arma::vec qs_i,ps_i:
     double Q;
     arma::mat R;
+    int T_index,t_now=0;
     std::function<double(double,double)> phid;
     dklcost(double _Q, arma::mat _R,int _K, int _X1,int _X2,std::function<double(double,double)> _phid,double _L1,double _L2,
         double _T,system *_sys){
       Q=_Q; R=_R; sys=_sys; K = _K; phid = _phid; T=_T; // initialize with Q, R, sys, phid, and the domain
       X1 = _X1; X2=_X2; L1 = _L1; L2 = _L2;
-      //Choose K samples over the domain [[0,2*L1],[0,2*L2]]
-      
+      T_index = T/sys->dt;
+      xfuture=arma::zeros<arma::mat>(sys->Xcurr.n_rows,T_index);
+      xpast=arma::zeros<arma::mat>(sys->Xcurr.n_rows,300/sys->dt);//initialize xpast to hold up to fiveminutes of data
+      domainsamps.set_size(2,K);
+      qs_i.set_size(K);ps_i.set_size(K);
+      resample();
     };
     double l (const arma::vec& x,const arma::vec& u,double ti);
     arma::vec dldx (const arma::vec&x, const arma::vec& u, double ti);
@@ -95,49 +105,34 @@ template<class system> double dklcost<system>::calc_cost (const arma::mat& x,con
       J1+=arma::as_scalar(LamK*pow((cktemp(k1,k2)-phik(k1,k2)),2.));
     };
   };J1 = Q*J1; //cout<<"ergodic cost "<<J1<<" ";
-  for (int i = 0; i<x.n_cols; i++){
+  /*for (int i = 0; i<x.n_cols; i++){
     arma::vec xproj = sys->proj_func(x.col(i));
     J1+=l(xproj,u.col(i),sys->tcurr+(double)i*sys->dt); 
-  };//cout<<"total cost "<<J1<<"\n";
+  };//cout<<"total cost "<<J1<<"\n";*/
 return J1;}
 
-template<class system> double dklcost<system>::gpast(const arma::mat& s){//g(s) defined for 0 to tcurr
+template<class system> double dklcost<system>::g(const arma::vec& s){//g(s) defined for 0 to tcurr
   double total = 0.;
-  int t_index = round(sys->tcurr/sys->dt);
-  for(int i = 0;i<t_index;i++{
+  for(int i = 0;i<t_now;i++{
     arma::vec si = s-sys->proj_func(xpast.col(i));
     total+=sys->dt*exp(-0.5*arma::as_scalar(si.t()*sigma.i()*si));
+  for(int j = 0;j<xfuture.n_cols;j++{
+    arma::vec sj = s-sys->proj_func(xpast.col(j));
+    total+=sys->dt*exp(-0.5*arma::as_scalar(sj.t()*sigma.i()*sj));
   };
+  return total;
 }
 
-template<class system> void ergodicost<system>::phikfunc(){//integrate for 0 to L1
-  cout<<trapint(phid)<<"\n";
-  #pragma omp parallel for 
-  for(int n=0;n<K;n++){
-    //#pragma omp parallel for
-    for(int m=0;m<K;m++){
-      int L1ind = 100; int L2ind = 100;
-      double d1 = 2*L1/L1ind;
-      double d2 = 2*L2/L2ind;
-      auto Fk = [&](double x1,double x2){
-        return phid(x1,x2)*cos(m*PI*(x1)/(2*L1))*cos(n*PI*(x2)/(2*L2))/hk(m,n);};
-      phik(m,n)=trapint(Fk);      
-    };
-  };
+template<class system> void dklcost<system>::resample(){
+  //Choose K samples over the domain [[-L1,L1],[-L2,L2]]
+  arma::vec domainsize = {2*L1,2*L2};
+  domainsamps=arma::diagmat(domainsize)*arma::randu<mat>(2,K);//generate uniform random samples from 0 to 2*L
+  domainsamps.each_col -= (0.5*domainsize);//shift samples to go from -L to L
+  
 }
-template<class system> arma::mat ergodicost<system>::ckfunc(const arma::mat& x){
-  arma::vec xproj;
-  arma::mat ck = arma::zeros<arma::mat>(K,K); 
-  #pragma omp parallel for 
-  for(int m=0;m<K;m++){
-    //#pragma omp parallel for
-    for(int n=0;n<K;n++){
-      ck(m,n) = eulint(x,m,n);
-      ck(m,n)=ck(m,n)/(sys->tcurr+x.n_cols*sys->dt);
-    };
-  };
-return ck;}
+
 template<class system> void dklcost<system>::xmemory(const arma::vec& x){
-  ckpast=(ckpast*sys->tcurr/(sys->tcurr+sys->dt))+ckfunc(x);//cout<<ckfunc(x)<<"\n";
+  xpast(t_now)= x;
+  t_now++;
 }
 #endif
